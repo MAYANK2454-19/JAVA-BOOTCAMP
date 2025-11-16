dfb3632b092c8b166e338dd5db85aff193a1aeab
MAYANK2454-19 <galaxymayank5@gmail.com>
2025-11-16 18:48:35 +0530

    Second commit

---
+++ p3.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+public class p3 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       
+       for(int i=0;i<=4;i++){
+         for(int j=4;j>=i;j--){
+        //     same can be done using
+        //     for(int i=4;i>=0;i--){
+        //  for(int j=0;j<=i;j++){
+        System.out.print("*");
+     
+       }
+        System.out.println();
+       }
+        
+     sc.close();
+    }
+    
+} 
